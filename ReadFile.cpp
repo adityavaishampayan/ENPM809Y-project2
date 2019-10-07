6d6c913eda9d5c2181daccdf3780176eb77bac47
BrianBock <bbock@terpmail.umd.edu>
2019-10-07 13:48:49 -0400

    removed pushback in maze assignment

--- ReadFile.cpp
+++ ReadFile.cpp
@@ -3,14 +3,11 @@
 #include <iostream>
 #include <fstream>
 #include <stdio.h>
-/**
- * The <code>ReadFile</code> function takes in a file path as a string (ex: "the_maze.txt").
- * It opens that text file and reads it, character by character, into a 2D vector (maze)
- */
-int ReadFile(const char* fileName);
 
 std::vector<std::vector<std::string>> maze(45, std::vector<std::string>(32));
 
+
+//https://thispointer.com/c-how-to-read-a-file-line-by-line-into-a-vector/
 
 int ReadFile(const char* fileName){
 // Open the File
@@ -27,7 +24,7 @@ int ReadFile(const char* fileName){
 
 	for(row=0; row<=line_count; row++){
 		for(col=0; col<col_total; col++){
-			maze.at(col).at(row).push_back(fgetc(pFile));
+			maze.at(col).at(row)=fgetc(pFile);
 		}fgetc(pFile);
 	}	
 	
